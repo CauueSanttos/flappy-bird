cd5aa40a0730cc247e72299f1578500bb94a1d87
Cauê Santos <cauedossantossilva@hotmail.com>
2020-12-27 17:29:51 -0300

    [feature] - initial setup

---
+++ game.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  console.log('[Cauê Santos] - Flappy Bird');
+
+  const sprites = new Image();
+  sprites.src = './sprites.png';
+
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('2d');
+}
